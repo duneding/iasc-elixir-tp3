07b09d194a76c9eaa2386aae8d2d2821739377c7
duneding <maj.dagostino@gmail.com>
2016-06-01 23:39:39 -0300

    iteracion 2. TODO add silent

--- lib/server.ex
+++ lib/server.ex
@@ -1,22 +1,44 @@
+defmodule User do
+  defstruct nickname: "", silent: [], pid: nil
+end
+
 defmodule Server do
-  def start do
-    spawn(fn -> loop end)
+  def start(clients) do
+    spawn(fn -> loop(clients) end)
   end
 
-  def loop do
+  def loop(clients) do
     receive do
-      {client, :connect, username} -> 
-      		IO.puts 'Se conecto usuario: #{username}. Proceso #{inspect client}'         
+      {client, :connect, nickname} ->
+          user = %User{nickname: nickname, pid: client}
+          clients = List.insert_at(clients, -1, user) 
+      		IO.puts 'Se conecto usuario: #{nickname}. Proceso #{inspect client}'         
+      {client, :disconnect} ->
+          clients = Enum.filter clients, fn x ->
+            Map.get(x,:pid) != client
+          end
+          IO.puts 'Clients: #{inspect clients}' 
+          clients = List.delete(clients, nil)          
+          IO.puts 'Se desconecto usuario con PID: #{inspect client}.'                   
+          loop(clients)
       {emisor, receptor, :visto} -> 
 	      	send emisor, {receptor, :visto}       		
+      {:getClients} -> 
+          IO.puts 'Clients: #{inspect clients}' 
       {emisor, receptor, :escribir, mensaje} -> 
 	      	IO.puts 'Escribe mensaje. Emisor #{inspect emisor} Receptor #{inspect receptor}. 
 	      			 Mensaje: #{mensaje}'
 	      	send receptor, {emisor, :typing}	
 	      	:timer.sleep(3* 1000);	 
 	      	send receptor, {self, emisor, :leer, mensaje}              	
+      {emisor, :silent, receptor} ->    
+          e = Enum.filter clients, fn x ->
+            Map.get(x,:pid) == emisor
+          end
+          e = List.first(e)
+
     end
-    loop
+    loop(clients)
   end
 
 end
@@ -33,9 +55,11 @@ defmodule Client do
 			send server, {emisor, self, :visto}
 		{emisor, :typing} -> 
 			IO.puts 'Usuario #{inspect emisor} esta escribiendo...'			
+    {emisor, :typing} -> 
+      IO.puts 'Usuario #{inspect emisor} esta escribiendo...'           
 		{receptor, :visto} ->
 			IO.puts 'Visto por #{inspect receptor}' 
-    end
+    end  
     loop
   end
 
